85468449ffa7de5cca7b212123d4d99a98d146ca
geoffroy.du-mesnil-du-buisson <geoffroy.du-mesnil-du-buisson.epita.fr>
2021-11-26 15:53:56 +0100

    nouv version echo avec clang format

--- geoffroy/echo.c
+++ geoffroy/echo.c
@@ -1,70 +1,65 @@
 #include <stdio.h>
 #include <string.h>
 
-int echo_e(char const *argv[]) {
-  for (int i = 2; argv[i]; i++) {
-    for (int j = 0; argv[i][j]; j++) {
-      if (argv[i][j + 1] != '\0' &&
-          (argv[i][j] == '\\' && argv[i][j + 1] == 'n')) {
+int echo(char const *argv[])
+{
+    int flag = 0;
+    if (argv[1] == NULL)
+    {
         printf("\n");
-        j++;
-      } else if (argv[i][j + 1] != '\0' &&
-                 (argv[i][j] == '\\' && argv[i][j + 1] == 't')) {
-        printf("\t");
-        j++;
-      } else if (argv[i][j + 1] != '\0' &&
-                 (argv[i][j] == '\\' && argv[i][j + 1] == '\\')) {
-        printf("\\");
-        j++;
-      } else
-        printf("%c", argv[i][j]);
+        return 0;
     }
-    if (argv[i + 1] != NULL) {
-      printf(" ");
+    else if (strcmp("-n", argv[1]) == 0)
+        flag = 2;
+    else if (strcmp("-e", argv[1]) == 0)
+        flag = 1;
+    int i = 1;
+    if (flag > 0)
+        i = 2;
+    for (; argv[i]; i++)
+    {
+        for (int j = 0; argv[i][j]; j++)
+        {
+            if (flag == 1)
+            {
+                if (argv[i][j + 1] != '\0'
+                    && (argv[i][j] == '\\' && argv[i][j + 1] == 'n'))
+                {
+                    printf("\n");
+                    j++;
+                }
+                else if (argv[i][j + 1] != '\0'
+                         && (argv[i][j] == '\\' && argv[i][j + 1] == 't'))
+                {
+                    printf("\t");
+                    j++;
+                }
+                else if (argv[i][j + 1] != '\0'
+                         && (argv[i][j] == '\\' && argv[i][j + 1] == '\\'))
+                {
+                    printf("\\");
+                    j++;
+                }
+                else
+                    printf("%c", argv[i][j]);
+            }
+            else
+                printf("%c", argv[i][j]);
+        }
+        if (argv[i + 1] != NULL)
+            printf(" ");
     }
-  }
-  return 0;
+    if (flag < 2)
+        printf("\n");
+    return 0;
 }
 
-int echo_n(char const *argv[]) {
-  for (int i = 2; argv[i]; i++) {
-    for (int j = 0; argv[i][j]; j++) {
-      printf("%c", argv[i][j]);
+int main(int argc, char const *argv[])
+{
+    if (argc == 8)
+    {
+        printf("haha je t'ais fais une blague\n");
     }
-    if (argv[i + 1] != NULL) {
-      printf(" ");
-    }
-  }
-  return 0;
-}
-
-int simple_echo(char const *argv[]) {
-  for (int i = 1; argv[i]; i++) {
-    for (int j = 0; argv[i][j]; j++) {
-      printf("%c", argv[i][j]);
-    }
-    if (argv[i + 1] != NULL) {
-      printf(" ");
-    }
-  }
-  printf("\n");
-  return 0;
-}
-
-int echo(int argc, char const *argv[]) {
-  if (argc < 2) {
-    printf("\n");
-  } else if (strcmp("-n", argv[1]) == 0) {
-    return echo_n(argv);
-  } else if (strcmp("-e", argv[1]) == 0) {
-    return echo_e(argv);
-  } else {
-    return simple_echo(argv);
-  }
-  return 0;
-}
-
-int main(int argc, char const *argv[]) {
-  return echo(argc, argv);
-  return 0;
+    return echo(argv);
+    return 0;
 }
