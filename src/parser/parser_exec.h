6b8c2cc7010cc1f5bc9a619f322abbda1d9b5533
geoffroy.du-mesnil-du-buisson <geoffroy.du-mesnil-du-buisson.epita.fr>
2021-11-30 20:27:29 +0100

    parser exec command

--- src/parser/parser_exec.h
+++ src/parser/parser_exec.h
@@ -11,13 +11,12 @@ void exec_ast_elif(struct ast *ast);
 
 void exec_ast_else(struct ast *ast);
 
-void exec_ast_command(struct ast *ast);
+int exec_ast_command(struct ast *ast);
 
 typedef void (*ast_exec_function)(struct ast *ast);
 
 void exec_ast(struct ast *ast);
 
 void execution(struct ast_main_root *ast);
-
 
 #endif
--- tests/test_geoffroy.c
+++ tests/test_geoffroy.c
@@ -4,6 +4,7 @@
 
 int main()
 {
+    printf("this is the test of an exec command\n", );
     char *argv[3] = { "echo", "geoffroy", NULL };
     return command_exec(argv);
     return 0;
