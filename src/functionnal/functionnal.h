f24483553eb82a04caded6ddf8a6417be4f508ee
Anna Barry <anna.barry@epita.fr>
2021-12-05 12:04:48 +0100

    features: global variables and env + tests

--- src/functionnal/functionnal.h
+++ src/functionnal/functionnal.h
@@ -1,12 +1,15 @@
-#pragma once
+#ifndef FUNCTIONNAL_H
+#define FUNCTIONNAL_H
 
 #include <sys/types.h> 
 #include <unistd.h> 
+#include <stdlib.h>
 
 struct variable
 {
     char *name;
     char *value;
+    struct variable *next;
 };
 
 struct environnement
@@ -16,11 +19,24 @@ struct environnement
     int nb_args; /// $#
     pid_t pid; /// $$
     int return_value; // $?
-    struct variables *var;
+    struct variable *var;
+    uid_t uid;
     int nb_variables;
-    int cap_variables;
 };
 
+/*
+ * Init function for environnement variables
+ */
 struct environnement *init_env(void);
 
+/*
+ * Print environnement variables
+ */
+void print_variables(struct environnement *new);
+
+/*
+ * Insert environnement variables
+ */
 void insert_variable(char *name, char *value, struct environnement *new);
+
+#endif /* !FUNCTIONNAL_H */
