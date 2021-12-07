5be5b72ceba8076526e7e7a31c548a90355c2e22
geoffroy.du-mesnil-du-buisson <geoffroy.du-mesnil-du-buisson.epita.fr>
2021-12-07 20:41:05 +0100

    test pour redir pipe command_exec et echo

--- src/commands/command_pipe.c
+++ src/commands/command_pipe.c
@@ -13,118 +13,118 @@
 #include "command.h"
 #include "echo.h"
 
-int exec_pipe(char *argv_left[], char *argv_right[], int count_left,
-              int count_right)
-{
-    int fd[2];
-    if (pipe(fd) == -1)
-        return 1;
-    int old_fd = dup(STDOUT_FILENO);
-    if (dup2(fd[1], STDOUT_FILENO) == -1)
-        fprintf(stderr, "Error with dup2 for fd1");
-    // close(fd[0]);
-    close(fd[1]);
-    command_exec(argv_left, count_left);
-    if (dup2(fd[0], STDIN_FILENO) == -1)
-        fprintf(stderr, "Error with dup2 for fd0");
-    close(fd[0]);
-    close(fd[1]);
-    command_exec(argv_right, count_right);
-    // close(fd[0]);
-    // close(fd[1]);
-    if (dup2(old_fd, STDOUT_FILENO) == -1)
-        fprintf(stderr, "Error with dup2");
-    fcntl(old_fd, F_SETFD, FD_CLOEXEC);
-    return 0;
-}
-// flush(stdout) for the builtin functions.
-
-int command_pipe(char *argv_left[], char *argv_right[], int count_left,
-                 int count_right)
-{
-    int old_fd = dup(STDOUT_FILENO);
-    int fd = 0;
-    if (dup2(fd, STDOUT_FILENO) == -1)
-        fprintf(stderr, "Error with dup2");
-    if (command_exec(argv_left, count_left) != 0)
-        return 127;
-    if (dup2(fd, STDIN_FILENO) == -1)
-        fprintf(stderr, "Error with dup2");
-    close(fd);
-    if (command_exec(argv_right, count_right) != 0)
-        return 127;
-    if (dup2(old_fd, STDOUT_FILENO) == -1)
-        fprintf(stderr, "Error with dup2");
-    fcntl(old_fd, F_SETFD, FD_CLOEXEC);
-    return 0;
-}
-
-int command_exec_pipe(char *argv_left[], char *argv_right[], int count_left,
-                      int count_right)
-{
-    int old_fd = dup(STDOUT_FILENO);
-    int fd[2];
-    if (pipe(fd) == -1)
-        return 1;
-    if (strcmp(argv_left[0], "echo") == 0)
-    {
-        dup2(fd[1], STDOUT_FILENO);
-        close(fd[1]);
-        close(fd[0]);
-        echo(argv_left, count_left);
-    }
-    else
-    {
-        int pid = fork();
-        if (pid < 0)
-        {
-            return 1;
-        }
-        if (pid == 0)
-        {
-            dup2(fd[1], STDOUT_FILENO);
-            close(fd[0]);
-            close(fd[1]);
-            execvp(argv_left[0], argv_left);
-        }
-        int wstatus1;
-        waitpid(pid, &wstatus1, 0);
-        if (WEXITSTATUS(wstatus1) == 127 || WEXITSTATUS(wstatus1) == 0)
-            return WEXITSTATUS(wstatus1);
-    }
-    if (strcmp(argv_right[0], "echo") == 0)
-    {
-        dup2(fd[0], STDIN_FILENO);
-        close(fd[0]);
-        close(fd[1]);
-        echo(argv_right, count_right);
-    }
-    else
-    {
-        int pid2 = fork();
-        if (pid2 < 0)
-            return 3;
-        if (pid2 == 0)
-        {
-            dup2(fd[0], STDIN_FILENO);
-            close(fd[0]);
-            close(fd[1]);
-            execvp(argv_right[0], argv_right);
-        }
-        close(fd[0]);
-        close(fd[1]);
-        int wstatus;
-        waitpid(pid2, &wstatus, 0);
-        if (WEXITSTATUS(wstatus) == 127 || WEXITSTATUS(wstatus) == 0)
-            return WEXITSTATUS(wstatus);
-
-        return 1;
-    }
-    if (dup2(old_fd, STDOUT_FILENO) == -1)
-        fprintf(stderr, "Error with dup2");
-    fcntl(old_fd, F_SETFD, FD_CLOEXEC);
-    return 0;
-}
+// int exec_pipe(char *argv_left[], char *argv_right[], int count_left,
+//               int count_right)
+// {
+//     int fd[2];
+//     if (pipe(fd) == -1)
+//         return 1;
+//     int old_fd = dup(STDOUT_FILENO);
+//     if (dup2(fd[1], STDOUT_FILENO) == -1)
+//         fprintf(stderr, "Error with dup2 for fd1");
+//     // close(fd[0]);
+//     close(fd[1]);
+//     command_exec(argv_left, count_left);
+//     if (dup2(fd[0], STDIN_FILENO) == -1)
+//         fprintf(stderr, "Error with dup2 for fd0");
+//     close(fd[0]);
+//     close(fd[1]);
+//     command_exec(argv_right, count_right);
+//     // close(fd[0]);
+//     // close(fd[1]);
+//     if (dup2(old_fd, STDOUT_FILENO) == -1)
+//         fprintf(stderr, "Error with dup2");
+//     fcntl(old_fd, F_SETFD, FD_CLOEXEC);
+//     return 0;
+// }
+// // flush(stdout) for the builtin functions.
+//
+// int command_pipe(char *argv_left[], char *argv_right[], int count_left,
+//                  int count_right)
+// {
+//     int old_fd = dup(STDOUT_FILENO);
+//     int fd = 0;
+//     if (dup2(fd, STDOUT_FILENO) == -1)
+//         fprintf(stderr, "Error with dup2");
+//     if (command_exec(argv_left, count_left) != 0)
+//         return 127;
+//     if (dup2(fd, STDIN_FILENO) == -1)
+//         fprintf(stderr, "Error with dup2");
+//     close(fd);
+//     if (command_exec(argv_right, count_right) != 0)
+//         return 127;
+//     if (dup2(old_fd, STDOUT_FILENO) == -1)
+//         fprintf(stderr, "Error with dup2");
+//     fcntl(old_fd, F_SETFD, FD_CLOEXEC);
+//     return 0;
+// }
+//
+// int command_exec_pipe(char *argv_left[], char *argv_right[], int count_left,
+//                       int count_right)
+// {
+//     int old_fd = dup(STDOUT_FILENO);
+//     int fd[2];
+//     if (pipe(fd) == -1)
+//         return 1;
+//     if (strcmp(argv_left[0], "echo") == 0)
+//     {
+//         dup2(fd[1], STDOUT_FILENO);
+//         close(fd[1]);
+//         close(fd[0]);
+//         echo(argv_left, count_left);
+//     }
+//     else
+//     {
+//         int pid = fork();
+//         if (pid < 0)
+//         {
+//             return 1;
+//         }
+//         if (pid == 0)
+//         {
+//             dup2(fd[1], STDOUT_FILENO);
+//             close(fd[0]);
+//             close(fd[1]);
+//             execvp(argv_left[0], argv_left);
+//         }
+//         int wstatus1;
+//         waitpid(pid, &wstatus1, 0);
+//         if (WEXITSTATUS(wstatus1) == 127 || WEXITSTATUS(wstatus1) == 0)
+//             return WEXITSTATUS(wstatus1);
+//     }
+//     if (strcmp(argv_right[0], "echo") == 0)
+//     {
+//         dup2(fd[0], STDIN_FILENO);
+//         close(fd[0]);
+//         close(fd[1]);
+//         echo(argv_right, count_right);
+//     }
+//     else
+//     {
+//         int pid2 = fork();
+//         if (pid2 < 0)
+//             return 3;
+//         if (pid2 == 0)
+//         {
+//             dup2(fd[0], STDIN_FILENO);
+//             close(fd[0]);
+//             close(fd[1]);
+//             execvp(argv_right[0], argv_right);
+//         }
+//         close(fd[0]);
+//         close(fd[1]);
+//         int wstatus;
+//         waitpid(pid2, &wstatus, 0);
+//         if (WEXITSTATUS(wstatus) == 127 || WEXITSTATUS(wstatus) == 0)
+//             return WEXITSTATUS(wstatus);
+//
+//         return 1;
+//     }
+//     if (dup2(old_fd, STDOUT_FILENO) == -1)
+//         fprintf(stderr, "Error with dup2");
+//     fcntl(old_fd, F_SETFD, FD_CLOEXEC);
+//     return 0;
+// }
 
 int pipe_exec(char *argv_left[], char *argv_right[], int count_left,
               int count_right)
@@ -174,13 +174,13 @@ int pipe_exec(char *argv_left[], char *argv_right[], int count_left,
 
 // int main()
 // {
-//     char *argv_left[3] = { "cat", "command.h" };
-//     char *argv_right[4] = { "cat", "-e" };
+//     char *argv_left[2] = { "ls" };
+//     char *argv_right[3] = { "cat", "-e" };
 //     // command_exec_pipe(argv_left, argv_right, 2, 3);
-//     pipe_exec(argv_left, argv_right, 2, 2);
+//     pipe_exec(argv_left, argv_right, 1, 2);
 //     // exec_pipe(argv_left, argv_right, 2, 3);
 //     // command_pipe(argv_left, argv_right, 2, 3);
 //     return 0;
 // }
-// cat command.c | cat -e
-// cat -e command.c
+// // cat command.c | cat -e
+// // cat -e command.c
--- tests/geoff/pipe_test.c
+++ tests/geoff/pipe_test.c
@@ -5,34 +5,34 @@
 
 int main(int argc, char const *argv[])
 {
-    char *argv0_left[1] = { "ls" };
-    char *argv0_right[2] = { "cat", "-e" };
-    char *argv1_left[1] = { "echo", "Hello" };
-    char *argv1_right[2] = { "cat", "-e" };
-    char *argv2_left[1] = { "echo", "Hallo" };
-    char *argv2_right[2] = { "tr", "a", "e" };
-    char *argv3_left[1] = { "echo", "a" };
-    char *argv3_right[2] = { "echo", "b" };
-    char *argv4_left[1] = { "ls" };
-    char *argv4_right[3] = { "tr", "a", "e" };
-    char *argv4_left[1] = { "ls" };
-    char *argv4_right[2] = { "tr", "a", "e" };
+    char *argv0_left[2] = { "ls" };
+    char *argv0_right[3] = { "cat", "-e" };
+
+    char *argv1_left[3] = { "echo", "Hello" };
+    char *argv1_right[3] = { "cat", "-e" };
+
+    char *argv2_left[3] = { "echo", "Hallo" };
+    char *argv2_right[4] = { "tr", "a", "e" };
+
+    char *argv3_left[3] = { "echo", "a" };
+    char *argv3_right[3] = { "echo", "b" };
+
+    char *argv4_left[2] = { "ls" };
+    char *argv4_right[4] = { "tr", "a", "e" };
     if (argc != 2)
         err(1, "Wrong args, try again with  : (./pipe test-*)\n");
     else
     {
         if (strcmp("test0", argv[1]) == 0) // simple echo
-            return echo(argv0_left, argv0_right, 1, 2);
+            return pipe_exec(argv0_left, argv0_right, 1, 2);
         else if (strcmp("test1", argv[1]) == 0)
-            return echo(argv1, 2);
+            return pipe_exec(argv1_left, argv1_right, 2, 2);
         else if (strcmp("test2", argv[1]) == 0)
-            return echo(argv2, 3);
+            return pipe_exec(argv2_left, argv2_right, 2, 3);
         else if (strcmp("test3", argv[1]) == 0)
-            return echo(argv3, 3);
+            return pipe_exec(argv3_left, argv3_right, 2, 2);
         else if (strcmp("test4", argv[1]) == 0)
-            return echo(argv4, 2);
-        else if (strcmp("test5", argv[1]) == 0)
-            return echo(argv5, 3);
+            return pipe_exec(argv4_left, argv4_right, 1, 3);
         else
             err(1, "Not implemented test");
     }
