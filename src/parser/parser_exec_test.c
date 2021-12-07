a407bc247b1f79afad954fb8e600628136bc94a2
Anna Barry <anna.barry@epita.fr>
2021-12-07 19:21:12 +0100

    testing for week 1

--- src/parser/parser_exec_test.c
+++ src/parser/parser_exec_test.c
@@ -1,5 +1,6 @@
 #include "parser_exec.h"
 #include "../commands/command.h"
+#include "../functionnal/functionnal.h"
 #include "parser.h"
 
 int main()
@@ -7,6 +8,7 @@ int main()
     struct lexer *lexer =
         lexer_new("if echo a then ls elif echo bebe then echo anna; fi");
     struct ast *ast = build_ast(lexer, NODE_ROOT);
+    struct environnement *env = init_env();
     printf("\n build ast with nb %i[ok]\n\n",
            ast->data.ast_main_root->nb_children);
     if (ast->type == NODE_ROOT)
@@ -14,7 +16,7 @@ int main()
         printf("node root\n");
     }
     printf("before pretty print\n");
-    int res_e = execution(ast);
+    int res_e = execution(ast, env);
     lexer_free(lexer);
     return 0;
 }
