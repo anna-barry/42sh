eb8e83fa9a3c3637ead72de5c3a3aef38a3af05d
geoffroy.du-mesnil-du-buisson <geoffroy.du-mesnil-du-buisson.epita.fr>
2021-12-14 01:22:44 +0100

    try to put a pipe in your a** I m sure it will heurt

--- src/commands/command_pipe.h
+++ src/commands/command_pipe.h
@@ -1,6 +1,8 @@
 #ifndef COMMAND_PIPE_H
 #define COMMAND_PIPE_H
 
+int pipe_ast(struct ast *ast, struct environnement *env);
+
 int pipe_exec(char *argv_left[], char *argv_right[], int count_left,
               int count_right);
 
