ee82d29812a3d61a809c62f382d07193018b7433
Anna Barry <anna.barry@epita.fr>
2021-12-14 22:10:33 +0100

    pipe works

--- src/commands/command_pipe.c
+++ src/commands/command_pipe.c
@@ -18,64 +18,21 @@
 
 int pipe_ast(struct ast *ast, struct environnement *env)
 {
-    printf("in pipe \n");
-    fflush(stdout);
     struct ast_pipe *pipe_ast = ast->data.ast_pipe;
     int fd[2];
-    // printf("ici\n");
-    // fflush(stdout);
     if (pipe(fd) == -1)
         return 1;
-    int pid = fork();
-    if (pid < 0)
-    {
-        return 1;
-    }
-    if (pid == 0)
-    {
-        // close(fd[0]);
-        dup2(fd[1], STDOUT_FILENO);
-        close(fd[0]);
-        close(fd[1]);
-        exec_ast(pipe_ast->left, env);
-        // printf("ici\n");
-        // fflush(stdout);
-    }
-    int pid2 = fork();
-    if (pid2 < 0)
-        return 3;
-    if (pid2 == 0)
-    {
-        // close(fd[1]);
-        dup2(fd[0], STDIN_FILENO);
-        close(fd[0]);
-        close(fd[1]);
-        // printf("before child second\n");
-        // fflush(stdout);
-        exec_ast(pipe_ast->right, env);
-        // printf("child second\n");
-        // fflush(stdout);
-        // putchar('a');
-    }
-    else
-    {
-        close(fd[0]);
-        close(fd[1]);
-        // putchar('a');
-        // printf("lalalalala\n");
-        // fflush(stdout);
-        kill(pid, SIGKILL);
-        waitpid(pid, NULL, 0);
-        // printf("la pute de w\n");
-        // fflush(stdout);
-        int wstatus;
-        kill(pid2, SIGKILL);
-        waitpid(pid2, &wstatus, 0);
-        // printf("la pute de sa grosse race\n");
-        // fflush(stdout);
-        if (WEXITSTATUS(wstatus) == 2 || WEXITSTATUS(wstatus) == 0)
-            return WEXITSTATUS(wstatus);
-        return 1;
-    }
-    return 0;
+    int fdout = dup(STDOUT_FILENO);
+    dup2(fd[1], STDOUT_FILENO);
+    int res = exec_ast(pipe_ast->left, env);
+    dup2(fdout, STDOUT_FILENO);
+    close(fdout);
+    close(fd[1]);
+    int fdin = dup(STDIN_FILENO);
+    dup2(fd[0], STDIN_FILENO);
+    res = exec_ast(pipe_ast->right, env);
+    dup2(fdin, STDIN_FILENO);
+    close(fdin);
+    close(fd[0]);
+    return res;
 }
