4e9d4dbee0122446c688b22fbf230b6f95c65537
Marine Thunet <marine.thunet@epita.fr>
2021-12-15 17:54:53 +0100

    exec return ok now i hope

--- src/commands/command.c
+++ src/commands/command.c
@@ -100,8 +100,6 @@ int command_exec(char *argv[], int count, struct environnement *env)
     else
     {
         int res = simple_command_exec(argv, count);
-        if (res == 127)
-            return 127;
         return res;
     }
 }
